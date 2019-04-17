59a55d554e92944bd122374ae97632d05a378ff2
sui <suitingwei@nanzhuxinyu.com>
2019-04-17 12:06:43 +0800

    1.learn the array function usage.

--- src/chapter1/main/funcs/array.go
+++ src/chapter1/main/funcs/array.go
@@ -2,14 +2,23 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"sort"
 )
 
 func main() {
+	arr := []int{1, 3, 5, 6}
+
+	result := searchInsert(arr, 5)
+
+	fmt.Println(result)
+}
+func sortArray() {
 
 	arr := []int{-4, -1, 0, 3, 10, 2}
 
-	result := []int{}
+	var result []int
+
 	for _, value := range arr {
 		result = append(result, value*value)
 	}
@@ -22,4 +31,19 @@ func main() {
 
 	fmt.Printf("%v\n", arr)
 
+}
+
+func searchInsert(nums []int, target int) int {
+	fmt.Println(len(nums))
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == target {
+			return i
+		} else if nums[i] >= target {
+			return i
+		}
+	}
+	return len(nums)
+}
+
+func maxiumSubquence(nums []int) int {
 }
