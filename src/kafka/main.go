b3504888ec68a9fbfbfff041f4b8878c7d36fe42
sui <suitingwei@nanzhuxinyu.com>
2019-04-26 18:51:13 +0800

    1.learn go kafka

--- src/kafka/main.go
+++ src/kafka/main.go
@@ -1,26 +1,46 @@
 package main
 
 import (
-	 "github.com/Shopify/sarama"
+	"github.com/Shopify/sarama"
 	"log"
+	"strconv"
 )
 
-func main (){
+func main() {
 	producer, err := sarama.NewSyncProducer([]string{"localhost:9092"}, nil)
 	if err != nil {
 		log.Fatalln(err)
 	}
+	//close the producer
 	defer func() {
 		if err := producer.Close(); err != nil {
 			log.Fatalln(err)
 		}
 	}()
 
-	msg := &sarama.ProducerMessage{Topic: "test", Value: sarama.StringEncoder("testing 123")}
-	partition, offset, err := producer.SendMessage(msg)
-	if err != nil {
-		log.Printf("FAILED to send message: %s\n", err)
+	messages := produceMessages()
+
+	//发送消息
+	sendErr := producer.SendMessages(messages)
+
+	if sendErr != nil {
+		log.Printf("FAILED to send message: %s\n", sendErr)
 	} else {
-		log.Printf("> message sent to partition %d at offset %d\n", partition, offset)
+		//log.Printf("> message sent to partition %d at offset %d\n", partition, offset)
+		log.Printf("Messages sent to partition somewhere\n")
 	}
+}
+
+func produceMessages() []*sarama.ProducerMessage {
+	var messages []*sarama.ProducerMessage
+
+	for i := 0; i < 100; i++ {
+		//要发送的消息
+		msg := &sarama.ProducerMessage{
+			Topic: "test", Value: sarama.StringEncoder("test message " + strconv.Itoa(i)),
+		}
+		messages = append(messages, msg)
+	}
+
+	return messages
 }
